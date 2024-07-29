7569d51098fed7b1fb64e80a7db692528bf94f8b
Fariskha Agustino Farendra <vanvaniaayoe@gmail.com>
2024-07-29 23:20:35 +0700

    create c++/BilanganDaduRandom.cpp

---
+++ c++/BilanganDaduRandom.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+
+  char lanjut;
+
+  while(true) {
+    cout << "Lempar dadu? (y/n)";
+    cin >> lanjut;
+
+    if (lanjut == 'y') {
+      cout << 1 + (rand() % 6) << endl;
+    } else if (lanjut == 'n') {
+      break;
+    } else {
+      cout << "Warning: ketik y atau n dong abang / kakak!!";
+    }
+  }
+}

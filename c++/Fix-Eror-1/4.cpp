fdb466b6416e09e69d24b8d5ad564a78df985e2a
Fariskha Agustino Farendra <fariskhaagustinofarendra@gmail.com>
2024-09-13 02:46:33 +0700

    create c++/Fix-Eror-1/4.cpp

---
+++ c++/Fix-Eror-1/4.cpp
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  int a, b, c, x, y;
+  
+  cout << "Masukan Nilai X=";
+  cin >> x;
+  cout <<"Masukan Nilai Y=";
+  cin >> y;
+
+  a = x & y;
+  b = x || y;
+  c = x ^ y;
+
+  cout << "\n";
+  cout << "Hasil dari " << x << "&" << y <<"=" << a << endl;
+  cout << "Hasil dari " << x << "|" << y << "=" << b << endl;
+  cout << "Hasil dari " << x << "^" << y << "="<< c << endl;
+
+  getchar();
+  return 0;
+}
